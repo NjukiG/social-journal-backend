e9b33373af747e88bc5ab800c014e8f67051e07d
George <gnjuki19@gmail.com>
2024-07-03 16:52:57 +0300

    Added category and journal models. Working on controllers now.

--- initializers/syncDB.go
+++ initializers/syncDB.go
@@ -6,7 +6,7 @@ import (
 )
 
 func SyncDatabase() {
-	err := DB.AutoMigrate(&models.User{})
+	err := DB.AutoMigrate(&models.User{}, &models.Category{}, &models.Journal{})
 
 	if err != nil {
 		log.Fatal("Failed to migrate model", err)
--- models/journalModels.go
+++ models/journalModels.go
@@ -12,15 +12,28 @@ const (
 // User Object/Model
 type User struct {
 	gorm.Model
-	FirstName string `gorm:"not null"`
-	LastName  string `gorm:"not null"`
-	Email     string `gorm:"unique;not null"`
-	Password  string `gorm:"not null"`
-	Role      Role   `gorm:"not null"`
+	FirstName  string     `gorm:"not null"`
+	LastName   string     `gorm:"not null"`
+	Email      string     `gorm:"unique;not null"`
+	Password   string     `gorm:"not null"`
+	Role       Role       `gorm:"not null"`
+	Categories []Category `gorm:"foreignKey:UserID"`
+	Journals   []Journal  `gorm:"foreignKey:UserID"`
 }
 
 // Category struct/ object
 type Category struct {
 	gorm.Model
-	Title string `gorm:"not null"`
+	Title    string    `gorm:"not null"`
+	Journals []Journal `gorm:"foreignKey:CategoryID"`
+	UserID   uint
+}
+
+type Journal struct {
+	gorm.Model
+	Title      string `gorm:"not null"`
+	Content    string `gorm:"not null"`
+	ImageURL   string
+	UserID     uint
+	CategoryID uint
 }
